4c012133ccc37951f771ac66a28083d4780b5672
Suvorov <mishok.su@gmail.com>
2022-07-13 23:08:38 +0700

    remove portlet

--- Portlets/farmers/farmers-portlet/src/main/java/com/test/farmers/DistrictNameException.java
+++
@@ -1,9 +0,0 @@
-package com.test.farmers;
-
-import com.liferay.portal.kernel.exception.PortalException;
-
-public class DistrictNameException extends PortalException {
-    public DistrictNameException() {
-        super("District name is empty");
-    }
-}
--- Portlets/farmers/farmers-portlet/src/main/java/com/test/farmers/FarmerCreateException.java
+++
@@ -1,9 +0,0 @@
-package com.test.farmers;
-
-import com.liferay.portal.kernel.exception.PortalException;
-
-public class FarmerCreateException extends PortalException {
-    public FarmerCreateException(String message) {
-        super(message);
-    }
-}
